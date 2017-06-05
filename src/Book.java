367174acbf58be6ed02b690655a5c40da00ea5e8
hdaylin <hdaylin@yahoo.com>
2017-06-05 17:03:44 -0400

    working

---
+++ src/Book.java
@@ -0,0 +1,63 @@
+
+public class Book { 
+	
+	private String sku; 
+	private String title;
+	private String author; 
+	private String description; 
+	private double price;
+	
+	public Book(){
+		
+	}
+	
+	public Book(String sku, String title, String author, String description, double price){
+		this.sku = sku; 
+		this.title = title; 
+		this.author = author; 
+		this.description = description; 
+		this.price = price; 
+	}
+	
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	} 
+	
+@Override 
+public String toString(){
+	return this.sku; 
+}
+	
+	
+	
+	
+
+}
---
+++ src/BookDB.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner; 
+
+public class BookDB {
+
+	public static void main(String[] args) {
+	
+		Scanner keyboard = new Scanner(System.in);
+		
+		ArrayList<Book> books = new ArrayList<Book>(); 
+		Book a = new Book("Java1001", "Head First java","Kathy Sierra and Bert Bates", "Easy to read Java workbook", 47.50);
+		books.add(a);
+		Book b = new Book("Java1002", "Thinking in Java", "Bruce Eckel", "	Details about Java under the hood", 20.00); 
+		books.add(b);
+		Book c = new Book("Orcl1003", "OCP: Oracle Certified Professional Java SE", "Jeanne Boyarsky", "Everything you need to know in one place", 45.00); 
+		books.add(c);
+		Book d = new Book("Python1004", "Automate the Boring Stuff with Python", "Al Sweigart", "Fun with Python", 10.50); 
+		books.add(d);
+		Book e = new Book("Zombie1005", "The Maker's Guide to the Zombie Apocalypse", "Simon Monk", "Defend Your Base with Simple Circuits, Arduino, and Raspberry Pi", 16.50); 
+		books.add(e);
+		Book f = new Book("Rasp1006", "Raspberry Pi Projects for the Evil Genius", "Donald Norris", "A dozen fiendishly fun projects for the Raspberry Pi!", 14.75); 
+		books.add(f); 
+		
+		
+	
+		for (Book book:books){  
+			
+			System.out.println(book.toString());
+		}
+		
+	
+	}
+
+}
